09c437e4fbc9449d49ada6be9f1f3443be9884f2
Pavel <pp@multiship.ru>
2013-10-01 18:59:50 +0400

    US-1093

--- multiship/objects/delivery.php
+++ multiship/objects/delivery.php
@@ -5,5 +5,6 @@ class MultiShip_Delivery extends MultiShip_Object
 {
   var $_prefix = "delivery_";
   var $_fields = array("direction", "delivery", "price", "pickuppoint", "to_ms_warehouse");
-  var $_critical = array("direction", "delivery", "price", "to_ms_warehouse");
+  var $_critical = array("direction", "delivery", "price", "to_ms_warehouse", "pickuppoint");
+  var $_not_empty = array("direction", "delivery", "price");
 }
