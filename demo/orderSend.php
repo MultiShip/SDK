3df4e5b141d5ba49de9172708b3d50c297c1d4f8
Potapov Max <mp@multiship.ru>
2013-11-12 19:13:44 +0400

    Убран лишний шаг при отправке заказа в службу доставки

--- demo/orderSend.php
+++ demo/orderSend.php
@@ -14,31 +14,11 @@ if ($_POST)
   /// Получаем рабочий экземпляр API
   $ms_api = MultiShip::init();
 
-  $order_id_1 = trim($_POST['order_id_1']);
-  $text_1 = $_POST['text_1'];
-  $parcel_id = null;
-  if ($_POST['prepareOrder'] && is_numeric($order_id_1))
-  {
-    /// подготавливаем заказ к отправке (помещаем его в отгрузку)
-    $ms_api->_data['order_id'] = $order_id_1;
-    $response = $ms_api->request('confirmSenderOrder');
-
-    if ($response->status == 'ok')
-    {
-      $text_1 = "Заказ подготовлен к отправке. Теперь его можно отправить.";
-      $parcel_id = $response->data->parcel_id;
-    }
-    else
-    {
-      $text_1 = $response->data;
-    }
-  }
-
   $order_id_2 = trim($_POST['order_id_2']);
   $text_2 = $_POST['text_2'];
   if ($_POST['sendOrder'] && is_numeric($order_id_2))
   {
-    /// получив номер заказа и поместив его в отгрузку, можем отправить заказ в службу доставки
+    /// получив номер заказа можем отправить заказ в службу доставки
     $ms_api->_data['order_id'] = $order_id_2; /// можно id нескольких заказов через запятую
     $ms_api->_data['date_shipment'] = time();
     $response = $ms_api->request('confirmSenderOrders');
@@ -58,14 +38,6 @@ if ($_POST)
 
 <link rel="stylesheet" href="css/multiship.css"/>
 <form method="POST">
-  <input type="text" name="order_id_1" placeholder="ID заказа" value="<?php echo isset($order_id_1) ? $order_id_1 : "" ?>"/>
-  <input name="prepareOrder" type="submit" value="Поместить заказ в отгрузку"/>
-  <input name="text_1" type="hidden" value="<?php echo $text_1 ?>"/>
-
-  <p>
-    <?php echo isset($order_id_1) ? "Заказ: " . $order_id_1 : "" ?><br>
-    <?php echo isset($text_1) ? $text_1 : "" ?>
-  </p>
   <input type="text" name="order_id_2" placeholder="ID заказа" value="<?php echo isset($order_id_2) ? $order_id_2 : "" ?>"/>
   <input name="sendOrder" type="submit" value="Отправить заказ в службу доставки"/>
   <input name="text_2" type="hidden" value="<?php echo isset($text_2) ? $text_2 : "" ?>"/>
